b8f6b5315bafb39f3be5e072bc0178c921beaa2b
ljh0608 <m98m@naver.com>
2025-12-01 05:24:26 +0900

    chore: vite aws배포설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      "ec2-44-210-84-152.compute-1.amazonaws.com", // ▲ 이 부분 추가!
+    ],
+  },
+});
